6af613431e9ba4aebe287d8e09b881462e4693d8
Bremerton <72746036+Bremerton@users.noreply.github.com>
2021-01-21 18:27:59 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+while (true) {
+    if (input.lightLevel() > 1) {
+        music.playMelody("C5 D5 A5 B5 D5 C5 C6 B5 B5 C6 B5 G5 E5 F5 A5 C5 A4 F5 D5 D5 C5 E4 F4 F4 A4 E4 G4 C4 G4 D4 F4 E5 E4 E5 B5 F5 E5 F5 D5 B4 D5 F5 E5 C5 E5 F5", 120)
+    }
+    
+}
